432cc47e14affa1bfa8c8610418cf4b39bca97d9
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-08 13:32:05 +0300

    Add AccountEventArgs

---
+++ BankLibrary/AccountStateHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    public delegate void AccountSateHandler(object sender, AccountEventArgs accountEventArgs);
+
+    public class AccountEventArgs
+    {
+        public string Message { get; private set;}
+        public decimal Sum { get; private set; }
+        public AccountEventArgs(string mess, decimal sum)
+        {
+            Message = mess;
+            Sum = sum;
+        }
+    }
+}
