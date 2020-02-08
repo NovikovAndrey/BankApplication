634e0f5cc90511ca3e2905d5607faaa642286652
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-08 14:12:47 +0300

    Add DemandAccount

---
+++ BankLibrary/DemandAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    class DemandAccount : Account
+    {
+        public DemandAccount(decimal sum, int percentage):base(sum, percentage)
+        {
+
+        }
+        public override void Open()
+        {
+            base.OnOpen(new AccountEventArgs($"Открыт новый счет до востребования {this.Id}", this.Sum));
+        }
+    }
+}
